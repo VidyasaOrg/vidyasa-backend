ff38861b673490ece1b9fd8e6c3545bdb6331dd8
M Farrel Danendra Rachim <13521048@std.stei.itb.ac.id>
2025-05-25 20:32:04 +0700

    feat: modify preprocessing tokens

--- app/utils/preprocessing.py
+++ app/utils/preprocessing.py
@@ -5,11 +5,7 @@ from nltk.stem import PorterStemmer
 stemmer = PorterStemmer()
 stop_words = set(stopwords.words("english"))
 
-def preprocess_text(text: str) -> str:
-    text = text.lower()
-    text = re.sub(r'[^a-z\s]', '', text)
-
-    tokens = text.split()
+def preprocess_text(tokens):
 
     processed_tokens = [stemmer.stem(word) for word in tokens if word not in stop_words]
 
