1fd6cc961d32d9b203847b506f4622df7a0c304a
akmaldika <13521070@mahasiswa.itb.ac.id>
2025-05-27 18:35:14 +0700

    feat!: refactor code

--- app/core/model.py
+++
@@ -1,9 +0,0 @@
-import os
-import openai
-from dotenv import load_dotenv
-
-class GPTQueryExpander:
-    def __init__(self, model_name="gpt-4.1"):
-        load_dotenv()
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.model = model_name
