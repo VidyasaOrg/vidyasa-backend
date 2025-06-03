22d4b27b1eb3696a2649fee96e44c2ba3463ba7c
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2025-06-04 00:16:48 +0700

    chore: add localhost:5173 @ CORS origins

--- app/core/config.py
+++ app/core/config.py
@@ -15,6 +15,9 @@ class Settings(BaseSettings):
         "http://localhost:5000",
         "http://127.0.0.1:5000",
 
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+
         "http://localhost:8000",
         "http://127.0.0.1:8000",
     ]
